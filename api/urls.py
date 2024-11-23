182a2934ad35d6d6750a3e18d3036c8b984037b7
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-11-23 17:29:39 +0200

    feat: Added order_by for titles and names. Added Query params for ArticleListView and AuthorListView

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.ArticleListAPIView.as_view(), name='article-list'),
-    path('authors/', views.AuthorListAPIView.as_view(), name='author-list'),
+    path('articles/', views.ArticleListView.as_view(), name='article-list'),
+    path('authorsa/', views.AuthorListView.as_view(), name='author-list'),
 ]
